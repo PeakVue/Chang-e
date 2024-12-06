6e30f9ecada813c907fe2452baf6578a1c15b76a
PeakVue <3055929217@qq.com>
2024-12-06 14:37:08 +0800

    24/12-6:实现内核线程

--- os/src/kernel/interrupt.c
+++ os/src/kernel/interrupt.c
@@ -61,7 +61,7 @@ static void general_intr_handler(uint8_t vec_nr) {
     if (vec_nr == 14) {	                                         // 若为Pagefault,将缺失的地址打印出来并悬停
         int page_fault_vaddr = 0; 
         asm ("movl %%cr2, %0" : "=r" (page_fault_vaddr));	     // cr2是存放造成page_fault的地址
-        put_str("\npage fault addr is ");put_int(page_fault_vaddr); 
+        put_str("\npage fault addr is ");put_int(page_fault_vaddr);put_char('\n');
     }
     put_str("#######################################################\n");
     while(1); // 能进入中断处理程序表明已经关闭了中断。
@@ -93,7 +93,7 @@ static void exception_init(void) {
     intr_name[12] = "#SS Stack Fault Exception";
     intr_name[13] = "#GP General Protection Exception";
     intr_name[14] = "#PF Page-Fault Exception";
-    intr_name[14] = "#AV Available";
+    intr_name[15] = "#AV Available";
     intr_name[16] = "#MF x87 FPU Floating-Point Error";
     intr_name[17] = "#AC Alignment Check Exception";
     intr_name[18] = "#MC Machine-Check Exception";
@@ -137,6 +137,11 @@ enum intr_status intr_get_status() {
    uint32_t eflags = 0; 
    GET_EFLAGS(eflags);
    return (EFLAGS_IF & eflags) ? INTR_ON : INTR_OFF;
+}
+
+/* 在中断处理程序数组第vector_no个元素中注册安装中断处理程序function */
+void register_handler(uint8_t vector_no, intr_handler function) {
+   idt_table[vector_no] = function; 
 }
 
 void idt_init() {
